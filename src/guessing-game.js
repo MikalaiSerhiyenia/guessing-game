d0d5095a5e1b003d20b549f1ebbc48bd565cd759
mikalaiserhiyenia <mikalai.serhiyenia@gmail.com>
2023-10-22 16:13:15 +0200

    feat: task complete (Sun, Oct 22, 2023 04:13:15 PM)

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,18 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  constructor() {}
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
+  guess() {
+    return Math.round((this.min + this.max) / 2);
+  }
+  lower() {
+    this.max = this.guess();
+  }
+  greater() {
+    this.min = this.guess();
+  }
 }
 
 module.exports = GuessingGame;
